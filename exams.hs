ef29cf906fbda84a4c918934a5b0ef376f96a5b2
Igor <Malheiros@MacBook-Pro-de-Igor.local>
2015-12-05 22:55:59 +0000

    exams answers

---
+++ exams.hs
@@ -0,0 +1,24 @@
+partialSums:: [Int] -> [Int]
+partialSums(x:[]) = x:[]
+partialSums (x:xs) = x: zipWith (+) (partialSums(x:xs)) (xs)
+
+from:: Int -> [Int]
+from n = iterate (+1) (n)
+
+powerLists:: [[Int]]
+powerLists = map (\n -> iterate(*n) (1) ) (from 1)
+
+-- For test
+--powerLists = map (\n -> take 5 (iterate(*n) (1) )) (from 1)
+
+powerLists':: [[Int]]
+powerLists' = [[y^x | x <- [1..]] | y <- [1..]]
+
+-- For test
+--powerLists' = [ take 5 ([y^x | x <- [1..]]) | y <- [1..]]
+
+--factorial 0 = 1
+--factorial n = n * factorial (n - 1)
+
+facTuples:: [(Int,Int)]
+facTuples = iterate ( \(n, fact) -> (n+1, (n+1)*fact) )  (1,1)
